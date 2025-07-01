09360e3e77d3499aebfdefd9bc43c2c05942963a
Nagaratna Hegde <nagaratnahegde@Nagaratnas-MacBook-Air.local>
2025-07-01 22:00:33 +0530

    made restaurant cards dynamic

--- App.js
+++ App.js
@@ -1,6 +1,238 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+let restaurantList = [
+  {
+    "@type": "ListItem",
+    position: 1,
+    item: {
+      "@type": "Restaurant",
+      name: "24@43 - The Oterra",
+      image:
+        "https://b.zmtcdn.com/data/pictures/7/51637/978343a8a1d283c36efb7ecf6ea02669_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "4.1",
+        reviewCount: "942",
+      },
+      url: "/bangalore/2443-the-oterra-electronic-city/info",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress: "43, Phase 1, Electronic City, Bangalore",
+      },
+      servesCuisine: "North Indian, Asian, Continental",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 2,
+    item: {
+      "@type": "Restaurant",
+      name: "Mangroove Taproom and Kitchen",
+      image:
+        "https://b.zmtcdn.com/data/pictures/9/21154439/e98ded422c47a4e1d11bc1fa8e40e009_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "4.4",
+        reviewCount: "1,372",
+      },
+      url: "/bangalore/mangroove-taproom-and-kitchen-electronic-city-bangalore/info",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress:
+          "25/6 Beratena Agrahara, Doddanagamangala Road, Naganathapura, Rayasandra, Electronic City, Bangalore",
+      },
+      servesCuisine: "Chinese, Asian, Italian",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 3,
+    item: {
+      "@type": "Restaurant",
+      name: "Ahara",
+      image:
+        "https://b.zmtcdn.com/data/pictures/0/20922890/c3c8e0d5fa01fc9bdf6f98524a1751b7_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "4.5",
+        reviewCount: "121",
+      },
+      url: "/bangalore/ahara-electronic-city-bangalore/info",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress:
+          "The Oterra, 43 Electronic City, Phase 1, Hosur Road, Electronic City, Bangalore",
+      },
+      servesCuisine: "North Indian, Beverages",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 4,
+    item: {
+      "@type": "Restaurant",
+      name: "Macaw By Stories",
+      image:
+        "https://b.zmtcdn.com/data/pictures/4/20707544/f86cd7384f723716b4ce88daa0928c8c_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "4.3",
+        reviewCount: "9,445",
+      },
+      url: "/bangalore/macaw-by-stories-bommanahalli-bangalore/info",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress:
+          "2224, 2225, AECS Layout, Near Singasandra, Hosur Main Road, Bommanahalli, Bangalore",
+      },
+      servesCuisine: "South Indian, North Indian, Chinese",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 5,
+    item: {
+      "@type": "Restaurant",
+      name: "East- The Oterra",
+      image:
+        "https://b.zmtcdn.com/data/pictures/4/58594/7e8342979da2f12ae790415b159f259f_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "4.2",
+        reviewCount: "185",
+      },
+      url: "/bangalore/east-the-oterra-electronic-city/info",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress: "43, Phase 1, Electronic City, Bangalore",
+      },
+      servesCuisine: "Chinese, Thai, Japanese",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 6,
+    item: {
+      "@type": "Restaurant",
+      name: "Bier Loft Brewing Co",
+      image:
+        "https://b.zmtcdn.com/data/pictures/4/21573564/307fbd547e3ff73a3b16fd1a35425f1f_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "4.3",
+        reviewCount: "661",
+      },
+      url: "/bangalore/bier-loft-brewing-co-electronic-city-bangalore/info",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress:
+          "4th Floor, Nishaanth Elegance, Neeladri Nagar, Electronics City Phase 1, Doddathoguru, Electronic City, Bangalore",
+      },
+      servesCuisine: "Continental, Asian, North Indian",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 7,
+    item: {
+      "@type": "Restaurant",
+      name: "Shift Lounge & Bar",
+      image:
+        "https://b.zmtcdn.com/data/pictures/3/20123043/1237d0d15387578cba93dbebf53a659a_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "4.3",
+        reviewCount: "1,787",
+      },
+      url: "/bangalore/shift-lounge-bar-bommanahalli-bangalore/info",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress:
+          "676, Survey 5/2, 5th Floor, Near More Mega Store, Singasandra, Hosur Road, Bommanahalli, Bangalore",
+      },
+      servesCuisine: "Continental, Bar Food, North Indian",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 8,
+    item: {
+      "@type": "Restaurant",
+      name: "Alloro - The Oterra",
+      image:
+        "https://b.zmtcdn.com/data/pictures/0/51670/69167db74d8a99fe1cadf8f5fb889025_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "4.6",
+        reviewCount: "162",
+      },
+      url: "/bangalore/alloro-the-oterra-electronic-city-bangalore/info",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress: "The Oterra, 43, Phase 1, Electronic City, Bangalore",
+      },
+      servesCuisine: "Italian, Beverages",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 9,
+    item: {
+      "@type": "Restaurant",
+      name: "The Terminus - The Oterra",
+      image:
+        "https://b.zmtcdn.com/data/pictures/4/53914/503afb3b0e074c4ef43d97627f642316_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "4.2",
+        reviewCount: "224",
+      },
+      url: "/the-terminus/info",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress: "The Oterra, Electronic City, Bangalore",
+      },
+      servesCuisine: "Finger Food, Continental, Beverages",
+    },
+  },
+];
 const styleCard = { backgroundColor: "#f0f0f0" };
 const Header = () => {
   return (
@@ -23,19 +255,21 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = ({ resName, cuisine }) => {
-  const {} = props;
+const RestaurantCard = ({ resData }) => {
+  console.log(resData);
+  const {
+    name,
+    servesCuisine,
+    aggregateRating: { ratingValue, reviewCount },
+    image,
+  } = resData.item;
   return (
     <div className="restaurant-card" style={styleCard}>
-      <img
-        className="res-logo"
-        alt="res-logo"
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/l7xlzfeh626mnmhecyy4"
-      ></img>
-      <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
-      <h4>4.4. stars</h4>
-      <h4>38 mins</h4>
+      <img className="res-logo" alt="res-logo" src={image}></img>
+      <h3>{name}</h3>
+      <h4>{servesCuisine}</h4>
+      <h4>{ratingValue}</h4>
+      <h4>{reviewCount}</h4>
     </div>
   );
 };
@@ -45,10 +279,9 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard
-          resName="Meghana Foods"
-          cuisine="Biryani, North Indian, Asian"
-        />
+        {restaurantList.map((restaurant) => (
+          <RestaurantCard key={restaurant.position} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
