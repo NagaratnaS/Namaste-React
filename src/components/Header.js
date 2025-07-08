12f71882abc521c82a0ab11aeb756d62e2e6813d
Nagaratna Hegde <nagaratnahegde@Nagaratnas-MacBook-Air.local>
2025-07-08 20:37:37 +0530

    added Li Component for routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,13 +11,18 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
           <li>
             <Link to="/about">About</Link>
           </li>
+
           <li>
             <Link to="/contact">Contact Us</Link>
           </li>
+
           <li>Cart</li>
           <button
             className="login"
