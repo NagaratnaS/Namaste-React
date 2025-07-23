f65fdc2738a4e1ae745487b4f965b4b3f2b34932
Nagaratna Hegde <nagaratnahegde@Nagaratnas-MacBook-Air.local>
2025-07-23 17:16:02 +0530

    fixed error in installing tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import "./index.css"; // Importing the main CSS file
 // import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+// This configuration file sets up Tailwind CSS for a React project.
+// It specifies the content files to scan for class names, extends the default theme, and includes
