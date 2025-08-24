63c7fb782c36aa3de32dbc05832ecbba0c6f2e06
Nagaratna Hegde <nagaratnahegde@Nagaratnas-MacBook-Air.local>
2025-08-24 21:57:00 +0530

    added test cases

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,16 @@
 const Contact = () => {
   return (
-    <div className="contact">
+    <div className="font-bold text-3xl p-4 m-4">
       <h1>Contact Us</h1>
       <p>If you have any questions, feel free to reach out!</p>
+      <form>
+        <input type="text" placeholder="name" className="border-2 m-2 p-2" />
+        <input type="email" placeholder="email" className="border-2 m-2 p-2" />
+        <input type="text" placeholder="message" className="border-2 m-2 p-2" />
+        <button type="submit" className="border-2 m-2 p-2">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,44 @@
+import { render } from "@testing-library/react";
+import Contact from "../Contact";
+import "@testing-library/react";
+
+describe("Contact Us Page test case", () => {
+  test("Should load Contact Us Component", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
+    //whenever we must check that the component is loaded and present or not then we use this
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("Should load Button inside Contact Us Component", () => {
+    render(<Contact />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
+});
+
+test("Should load Submit Button inside Contact Us Component", () => {
+  render(<Contact />);
+  const button = screen.getByText("Submit");
+  expect(button).toBeInTheDocument();
+});
+
+test("Should load input name inside Contact Us Component", () => {
+  //render
+  render(<Contact />);
+  const inputName = screen.getByPlaceholderText("name");
+  //assert
+  expect(inputName).toBeInTheDocument();
+});
+
+it("Should load all input boxes inside Contact Us Component", () => {
+  render(<Contact />);
+  //query
+  const inputBoxes = screen.getAllByRole("textbox");
+  //console.log(inputBoxes.length)
+  //expect(inputBoxes.length).not.toBe(2);
+  expect(inputBoxes.length).toBe(2);
+});
+
+//it and test both are same, they are just aliases of each other
+//can have multiple describe blocks
