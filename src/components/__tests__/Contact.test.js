d9b00fdb2b0d8fe0c4653b31b49cdb532729898e
Nagaratna Hegde <nagaratnahegde@Nagaratnas-MacBook-Air.local>
2025-08-27 21:24:12 +0530

    added integration tests

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -3,6 +3,22 @@ import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
 describe("Contact Us Page test case", () => {
+  beforeAll(() => {
+    console.log("Before All");
+  });
+
+  beforeEach(() => {
+    console.log("Before Each");
+  });
+
+  afterEach(() => {
+    console.log("After Each");
+  });
+
+  afterAll(() => {
+    console.log("After All");
+  });
+
   test("Should load Contact Us Component", () => {
     render(<Contact />);
     const heading = screen.getByRole("heading");
